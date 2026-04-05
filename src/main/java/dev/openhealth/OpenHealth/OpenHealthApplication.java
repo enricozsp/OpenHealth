e37b8a6a46fb5f85d5dc0211d3052c2d2add1b61
Enrico <enricoaffonso@gmail.com>
2026-04-05 19:19:53 -0300

    first commit

---
+++ src/main/java/dev/openhealth/OpenHealth/OpenHealthApplication.java
@@ -0,0 +1,13 @@
+package dev.openhealth.OpenHealth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenHealthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenHealthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/openhealth/OpenHealth/OpenHealthApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.openhealth.OpenHealth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenHealthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
